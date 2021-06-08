bf573eefd6d23e047773d16da3789b9155f7cb0a
ktkim <ktkim0111@gmail.com>
2021-06-08 22:39:40 +0900

    리모트 저장소 변경

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,38 +1,42 @@
-import React from "react";
-import DetailPresenter from "./DetailPresenter";
-import { moviesApi, tvApi } from "../../api";
+import React from 'react';
+import DetailPresenter from './DetailPresenter';
+import { moviesApi, tvApi } from '../../api';
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
     const {
-      location: { pathname }
+      location: { pathname },
     } = props;
     this.state = {
       result: null,
       error: null,
       loading: true,
-      isMovie: pathname.includes("/movie/")
+      isMovie: pathname.includes('/movie/'),
     };
   }
 
   async componentDidMount() {
     const {
       match: {
-        params: { id }
+        params: { id },
       },
-      history: { push }
+      history: { push },
     } = this.props;
     const { isMovie } = this.state;
     const parsedId = parseInt(id);
     if (isNaN(parsedId)) {
-      return push("/");
+      return push('/');
     }
     let result = null;
     try {
       if (isMovie) {
+        // 이렇게 하면 위의 result가 아닌 다른 result 변수 생성
+        // const { data: result } = await moviesApi.showDetail(parsedId);
         ({ data: result } = await moviesApi.movieDetail(parsedId));
       } else {
+        // 이렇게 하면 위의 result가 아닌 다른 result 변수 생성
+        // const { data: result } = await tvApi.showDetail(parsedId);
         ({ data: result } = await tvApi.showDetail(parsedId));
       }
     } catch {
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,38 @@
-export default () => "Search";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  font-size: 28px;
+  width: 100%;
+`;
+
+const SearchPresenter = ({ movieResult, tvResult, searchTerm, loading, error, handleSubmit, updateTerm }) => (
+  <Container>
+    <Form onSubmit={handleSubmit}>
+      <Input placeholder="Search Movies or TV Shows..." value={searchTerm} onChange={updateTerm}></Input>
+    </Form>
+  </Container>
+);
+
+SearchPresenter.propTypes = {
+  movieResult: PropTypes.array,
+  tvResult: PropTypes.array,
+  searchTerm: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired,
+  updateTerm: PropTypes.func.isRequired,
+};
+
+export default SearchPresenter;
